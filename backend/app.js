fe94b9705227dbd3e9a58736bcfee04596743787
Sujoy Manna <62413879+sujoy-2811@users.noreply.github.com>
2026-04-30 23:43:24 +0530

    feat: add morgan package

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import morgan from 'morgan'
 
 import authRoutes from './src/routes/auth.js'
 import employeeRoutes from './src/routes/employees.js'
@@ -10,6 +11,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(morgan('dev'))
 
 app.use('/auth', authRoutes)
 app.use('/employees', employeeRoutes)
